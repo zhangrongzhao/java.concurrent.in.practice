a0a07860a1d13ec1b2241c23a97113971b7e7bb6
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-01-07 17:40:36 +0800

    典型的线程池工作者线程结构

---
+++ src/main/java/com/zrz/chapter07/TypicalTask.java
@@ -0,0 +1,17 @@
+package com.zrz.chapter07;
+
+public class TypicalTask extends Thread {
+
+//    public void run() {
+//        Throwable thrown=null;
+//        try{
+//            while(!isInterrupted()){
+//                runTask(getTaskFromWorkQueue());
+//            }
+//        }catch(Throwable e){
+//            thrown=e;
+//        }finally {
+//            threadExited(this,thrown);
+//        }
+//    }
+}
